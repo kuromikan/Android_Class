933f0dfc37fb4e672c218158ec63c12b9ae08c40
YAO,WEI <gamania54088@gmail.com>
2018-08-13 17:07:12 +0800

    Add files via upload

---
+++ helloworld/src/MyJavaPackage/Wash.java
@@ -0,0 +1,14 @@
+package MyJavaPackage;
+
+public class Wash {
+	public void tooth() {
+		System.out.println("¨ê¤ú");
+	}
+	public void face() {
+		System.out.println("¬~Áy");
+	}
+	public void body() {
+		System.out.println("¬~¾þ");
+	}
+
+}
---
+++ helloworld/src/helloworld/Eat.java
@@ -0,0 +1,20 @@
+package helloworld;
+import MyJavaPackage.Wash;
+
+public class Eat {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		eatBreakfast();
+		Eat obj1 = new Eat();
+		obj1.eatLunch();
+		Wash obj2 = new Wash();
+		obj2.tooth();
+	}
+
+	public static void eatBreakfast() {		
+		System.out.println("¦Y¦­À\ªü");
+	}
+	public void eatLunch() {		
+		System.out.println("¦Y¤ÈÀ\ªü");
+	}
+}
